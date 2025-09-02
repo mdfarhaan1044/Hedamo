ec7f8458c1d9aeb8e149581fd5b87e77cb8dcc8a
mdfarhaan1044 <mdfarhaan1044@gmail.com>
2025-09-02 06:08:58 +0530

    Brand design

--- src/components/Brand/brand.jsx
+++ src/components/Brand/brand.jsx
@@ -11,7 +11,7 @@ export default function Brand({ product }) {
  px-8 py-6 flex items-center justify-center transition-all duration-300 hover:scale-105 hover:shadow-[inset_5px_5px_10px_#bebebe,inset_-5px_-5px_10px_#ffffff]"
     >
       <h1 className="text-lg md:text-xl font-medium text-white tracking-wide">
-        <span className="font-bold">Brand:</span> {product.brand}
+        <span className="font-bold text-black">Brand:</span> {product.brand}
       </h1>
     </div>
   );
